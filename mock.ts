2669993f46116ae906724aedf98519868792c7ad
geunheejung <geunhee0212@gmail.com>
2022-05-22 17:57:18 +0900

    eslint, prettier 적용.

--- mock.ts
+++ mock.ts
@@ -1,10 +1,20 @@
-export interface IAccount { id: string, pw: string, userId: string };
+export interface IAccount { id: string, pw: string, userId: string }
 
 export interface IUser { id: string, nickname: string }
-export interface IDB {
-  account: IAccount[],
-  user: IUser[]
 
+interface IPost {
+  userId: string;
+  thumbnail: string;
+  title: string;
+  content: string;
+  tag: string[];
+  date: string;
+  commentId: string;
+}
+export interface IDB {
+  account: IAccount[];
+  user: IUser[];
+  post: IPost[];
 }
 
 export const db: IDB = {
@@ -20,6 +30,17 @@ export const db: IDB = {
       id: '1',
       nickname: 'geuni',
         
+    }
+  ],
+  post: [
+    {
+      userId: '1',
+      thumbnail: '',
+      title: '',
+      content: '',
+      tag: [''],
+      date: '',
+      commentId: '1'
     }
   ]
 }
--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,9 @@ const app = (() => {
   app.use(cors());
   app.use(express.json());
 
-  app.listen(process.env.PORT, () => { console.log('listening on port', process.env.PORT); });
+  app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT);
+  });
 
   return app;
 })();
